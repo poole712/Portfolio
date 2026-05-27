72ec6895449111eeb958677a2c21ea62ee25e9c5
poole712 <54291199+poole712@users.noreply.github.com>
2026-05-27 15:08:58 +1200

    Fixed Layout.tsx to use React Vercel Analytics

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout() {
   return (
